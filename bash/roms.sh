1f962d478038b88ba14ef891d725a2b66838af22
Sean Mayo <sean.mayo@matsonfruit.com>
2016-11-09 13:53:07 -0800

    started working on roms autodownload script

---
+++ bash/roms.sh
@@ -0,0 +1,9 @@
+#sudo apt-get update
+#sudo apt-get -y dist-upgrade
+#sudo apt-get -y autoremove
+#sudo apt-get clean
+#sudo apt-get install -y wget
+#sudo apt-get install -y unrar
+wget --limit-rate=256k -O /mnt/z/nes/roms.rar "http://208.77.22.75/happyxhJ1ACmlTrxJQpol71nBc/Fullsets/GoodNES3.14.rar"
+#unrar /mnt/z/nes/roms.rar
+
