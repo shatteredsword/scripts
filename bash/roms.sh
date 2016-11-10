6e14f808d30b59edd9350c5af8b0edbdc39f7ca8
Sean Mayo <sean.mayo@matsonfruit.com>
2016-11-09 16:33:04 -0800

    more roms stuff

--- bash/roms.sh
+++ bash/roms.sh
@@ -4,6 +4,10 @@
 #sudo apt-get clean
 #sudo apt-get install -y wget
 #sudo apt-get install -y unrar
-wget --limit-rate=256k -O /mnt/z/nes/roms.rar "http://208.77.22.75/happyxhJ1ACmlTrxJQpol71nBc/Fullsets/GoodNES3.14.rar"
-#unrar /mnt/z/nes/roms.rar
+#sudo apt-get install -y p7zip-full
+cd /mnt/z/nes/
+#wget --limit-rate=256k -O /mnt/z/nes/roms.rar "http://208.77.22.75/happyxhJ1ACmlTrxJQpol71nBc/Fullsets/GoodNES3.14.rar"
+#unrar e roms.rar
+#rm roms.rar
+#7za x "*.7z"
 
