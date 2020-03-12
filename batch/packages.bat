ca49c0f99d918d5accbe4543c2759e349e2e4039
Sean Mayo <blackknight452@gmail.com>
2020-03-11 20:54:57 -0700

    new computer

---
+++ batch/packages.bat
@@ -0,0 +1,26 @@
+choco install -y 7zip
+choco install -y autohotkey
+choco install -y firefox
+choco install -y flashplayerplugin
+choco install -y git
+choco install -y google-backup-and-sync
+:choco install -y googlechrome
+:choco install -y inkscape
+:choco install -y jdk8
+choco install -y office365proplus
+:oracle-sql-developer
+choco install -y paint.net
+:choco install -y partitionwizard
+:choco install -y pencil
+:choco install -y pgadmin4
+choco install -y powertoys
+:choco install -y qbittorrent
+choco install -y signal
+:choco install -y sql-server-managemment-studio
+choco install -y steam
+choco install -y vlc
+choco install -y vnc-viewer
+choco install -y vscode
+choco install -y win32diskimager
+:choco install -y windirstat
+PAUSE
